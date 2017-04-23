a9b3731fc73f963fbb3997021dfeecfc8008c410
hlwang <pkuxuheyuan@163.com>
2017-04-23 22:49:28 +0800

    add tab-bar

---
+++ dist/js/test.js
@@ -0,0 +1,28 @@
+
+$(function () {
+	var timer = undefined;
+	var tabBar = $('#tab-bar');
+	function stopTimer() {
+		if (timer !== undefined) {
+			clearInterval(timer);
+			timer = undefined;
+		}
+	}
+	function startTimer() {
+		stopTimer();
+		timer = setInterval(function() {
+			var next = tabBar.find('li.active').next('li')
+			if (!next.length)
+				next = tabBar.find('li').first()
+			next.find('[data-toggle="tab"]').tab('show');
+		}, 5000);
+	}
+
+	tabBar.on('mouseenter', '[data-toggle="tab"]', function () {
+		stopTimer();
+		$(this).tab('show');
+	});
+	tabBar.hover(stopTimer, startTimer);
+	$('#tab-content').hover(stopTimer, startTimer);
+	startTimer();
+})
